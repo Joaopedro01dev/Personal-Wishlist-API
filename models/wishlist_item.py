b7e5e5a99e0ccd2f6a390f8934bfee802d449911
Joao Pedro <joaopedrocampossousa888@gmail.com>
2026-06-22 20:44:06 -0300

    Agora vai kk

--- models/wishlist_item.py
+++ models/wishlist_item.py
@@ -1,4 +1,5 @@
 from factory import db
+from sqlalchemy import text
 
 class WishlistItem(db.Model):
     __tablename__ = "WishlistItem"
@@ -7,7 +8,7 @@ class WishlistItem(db.Model):
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.Text, nullable=True)
     link = db.Column(db.String(128), nullable=True)
-    purchased = db.Column(db.Boolean, nullable=False, default=False)
+    purchased = db.Column(db.Boolean, nullable=False, default=False, server_default=text("0"))
     sort_order = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
